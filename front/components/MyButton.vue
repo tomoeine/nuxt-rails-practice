2b272997213491ccb81a308d01197dd60f39ab08
Tomoe Sawai <tomoe.teshima@gmail.com>
2021-03-01 14:58:52 +0900

    [add] answer for problem 3

---
+++ front/components/MyButton.vue
@@ -0,0 +1,27 @@
+<template>
+  <button class="button" @click="$emit('click')">{{ value }}</button>
+</template>
+
+<script>
+export default {
+  props: {
+    value: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+.button {
+  background-color: $color_blue;
+  color: $color_white;
+  font-size: 16px;
+  line-height: 30px;
+  font-weight: 700;
+  border-radius: 10px;
+  width: 160px;
+  height: 30px;
+}
+</style>
--- front/pages/authors/index.vue
+++ front/pages/authors/index.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <h2>著者一覧</h2>
+    <my-button value="新規登録" @click="onCreateButtonClick"></my-button>
     <ul v-for="author in authors" :key="author.id">
       <li>{{ author.name }}</li>
     </ul>
@@ -8,7 +9,10 @@
 </template>
 
 <script>
+import MyButton from "@/components/MyButton.vue";
+
 export default {
+  components: { MyButton },
   layout: "defualt",
   data() {
     return {
@@ -22,6 +26,11 @@ export default {
     } catch (e) {
       console.error(e);
     }
+  },
+  methods: {
+    onCreateButtonClick() {
+      this.$router.push("/authors/create");
+    },
   },
 };
 </script>
