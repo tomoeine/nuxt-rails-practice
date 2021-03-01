402c600ea51cad376d28af01120ca9e3e334fd61
Tomoe Sawai <tomoe.teshima@gmail.com>
2021-03-01 14:21:26 +0900

    [add] answer for problem 2

--- front/pages/authors/index.vue
+++ front/pages/authors/index.vue
@@ -1,14 +1,27 @@
 <template>
   <div>
     <h2>著者一覧</h2>
+    <ul v-for="author in authors" :key="author.id">
+      <li>{{ author.name }}</li>
+    </ul>
   </div>
 </template>
 
 <script>
 export default {
   layout: "defualt",
-  created() {
-    console.log("著者一覧");
+  data() {
+    return {
+      authors: [],
+    };
+  },
+  async created() {
+    try {
+      const { data } = await this.$axios.get("/authors");
+      this.authors = data;
+    } catch (e) {
+      console.error(e);
+    }
   },
 };
 </script>
